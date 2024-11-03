42d04c476190aaf7a48386e1f331c279f5140208
Gvile <guillaumevillemart@gmail.com>
2024-11-03 19:52:03 +0100

    create home page

---
+++ Demineur/Demineur.Client/Components/Pages/Home.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Demineur.Client.Components.Pages;
+
+public class HomeBase : ComponentBase
+{
+    [Inject] private NavigationManager _navigationManager { get; set; } = null!;
+    
+    public void GoToGamePage(int id)
+    {
+        _navigationManager.NavigateTo($"/Game/{id}");
+    }
+}
